c4f8bb7c562f0ca7794b8b32d72d975b70e95064
Sumit108-9 <sumtikamat416@gmail.com>
2026-04-29 12:39:17 +0530

    clean repo

--- backend/routes/googleAuth.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { googleSignIn } from '../controllers/googleAuth.controller.js';
-
-const router = express.Router();
-
-// Google Sign In
-router.post('/google', googleSignIn);
-
-export default router;
